4fee27b7a7e8012eaed898d71ad0a8041e77b07e
Stefan Pristoleanu <stefan.pristoleanu@student.manchester.ac.uk>
2019-01-06 21:27:05 +0000

    Updated Readme.md

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # rideways_technical_test
 
 # Setup
+
+My project is built with Maven and can be loaded in any Java IDE (I have developed it in Netbeans).
+Below I will put he console commands to compile and run the project.
+
 ```
  git clone https://github.com/StefanPristoleanu/rideways_technical_test.git
  cd rideways_technical_test
@@ -10,17 +14,42 @@
 ## Part 1 - Console application: 
 
 ### a) Console application to print the search results for Dave's Taxis
+```  
+  mvn install dependency:copy-dependencies 
+  cd target/classes
+  java -cp ".:../dependency/*" bookinggo.rides.RidesAppConsole 1 pickup=51.470020,-0.454295\&dropoff=51.00000,1.0000 2
 ```
-  java -jar target/rides-0.0.1.jar bookinggo.rides.RidesAppConsole
+ - output example:
+Dave's sorted descending list: 
+LUXURY - 895796
+LUXURY_PEOPLE_CARRIER - 867688
+STANDARD - 72568
 
-```
+
+__Note:__ the parameters for RidesAppConsole are:
+  - first param is 1 or 2 based on part1.a or part1.b requests
+  - second param are the pickup and dropoff coordinates as in the example
+  - the third param is the requested number of passengers (is not used for part1.a)
+
 ### b) Console application to filter by number of passengers
+```
+    java -cp ".:../dependency/*" bookinggo.rides.RidesAppConsole 2 pickup=51.470020,-0.454295\&dropoff=51.00000,1.0000 4
+```
+ - output example:
+Final descending list: 
+LUXURY - JEFF - 97866
+PEOPLE_CARRIER - JEFF - 103555
+EXECUTIVE - DAVE - 126396
+LUXURY_PEOPLE_CARRIER - DAVE - 584727
+STANDARD - ERIC - 779779
 
+--
 
 ## Part 2 - RESTful API
 
   Start server with command:
-
+  - assuming that you followed the commands in the project root folder (with pom.xml)
+  
 ```
  java -jar target/rides-0.0.1.jar 
 ```
--- src/main/java/bookinggo/rides/RidesAppConsole.java
+++ src/main/java/bookinggo/rides/RidesAppConsole.java
@@ -16,23 +16,25 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 /**
- * 
+ *
  * @author stefan
  */
-
 public class RidesAppConsole {
 
+    //Map in which I store the capacity of each car with teh carType as a key
     private static final HashMap<String, Integer> carCapacityMap = new HashMap<String, Integer>();
 
-    //args: pickup=51.470020,-0.454295&dropoff=51.00000,1.0000 5
+    //args for dave descending:1 pickup=51.470020,-0.454295&dropoff=51.00000,1.0000 5
+    //args for best cars: 2 pickup=51.470020,-0.454295&dropoff=51.00000,1.0000 5
     public static void main(String[] args) {
         processRequest(args);
     }
 
+    //Connects to an URL and returns the http response in JsonNode format
     public static HttpResponse<JsonNode> connectURL(String[] args, String supplierName) throws UnirestException {
         String supplierAPI = "https://techtest.rideways.com/" + supplierName;
         supplierAPI += '?' + args[1];
-        
+
         // first parameter is the connection timeout (the time it takes to connect to a server)
         //second parameter is  the socket timeout (the time it takes to receive data)
         //when socket timeout is 2000 more than 50% requests have timeout.
@@ -45,6 +47,11 @@ public class RidesAppConsole {
         return jsonResponse;
     }
 
+    //Handle the request based on the paramaters given:
+    //Call each API
+    //Filter results by the number of passengers
+    //Sort the List of cars types and suppliers by price in ascending order
+    //Select only the cheapest of each car type into a separate list and display it
     protected static boolean processRequest(String[] args) {
         if (args == null || args.length < 3) {
             System.out.println("No pickup and dropoff found. Exiting");
@@ -53,17 +60,13 @@ public class RidesAppConsole {
         System.out.println("App is running with args: " + Arrays.toString(args));
         List<CarPrice> carList = new ArrayList<>();
 
-        //if the first argument is 1, we attempt step1 phase A: The response to Dave's Taxis in descending order
         System.out.println("PassengersNo: " + args[2]);
         int passengersNo = Integer.valueOf(args[2]);
-
-        //String daveAPI = "https://techtest.rideways.com/dave?pickup=51.470020,-0.454295&dropoff=51.00000,1.0000";
-        //String daveAPI = "https://techtest.rideways.com/dave";
-        //String ericAPI = "https://techtest.rideways.com/eric?pickup=51.470020,-0.454295&dropoff=51.00000,1.0000";
-        //String jeffAPI = "https://techtest.rideways.com/jeff?pickup=51.470020,-0.454295&dropoff=51.00000,1.0000";
+        
         try {
             HttpResponse<JsonNode> jsonResponse = connectURL(args, "dave");
             if (args[0].equals("1")) {
+                //Get the response to Dave's Taxis in descending order and exit early
                 searchResultForDave(jsonResponse, carList);
                 return true;
             }
@@ -91,16 +94,19 @@ public class RidesAppConsole {
     }
 
     public static void searchResultForDave(HttpResponse<JsonNode> jsonResponse, List<CarPrice> carList) {
-        //error cases received from suppliers, will abort method
+        //Error cases received from suppliers, will abort the method
         if (jsonResponse.getBody().getObject().has("error")) {
             return;
         }
+        
         JSONArray daveResponse = jsonResponse.getBody().getObject().getJSONArray("options");
+        //Hold the carType and carPrice in a list        
         for (int i = 0; i < daveResponse.length(); i++) {
             JSONObject job = daveResponse.getJSONObject(i);
             CarPrice cp = new CarPrice(job.getString("car_type"), job.getInt("price"), "Dave");
             carList.add(cp);
         }
+        //Sort the list in descending order
         sortCarsDesc(carList);
         System.out.println("Dave's sorted descending list: ");
         for (int i = 0; i < carList.size(); i++) {
@@ -113,6 +119,7 @@ public class RidesAppConsole {
         if (jsonResponse.getBody().getObject().has("error")) {
             return;
         }
+        //Fill the map only once
         if (carCapacityMap.isEmpty()) {
             carCapacityMap.put("STANDARD", 4);
             carCapacityMap.put("EXECUTIVE", 4);
@@ -121,9 +128,10 @@ public class RidesAppConsole {
             carCapacityMap.put("LUXURY_PEOPLE_CARRIER", 6);
             carCapacityMap.put("MINIBUS", 16);
         }
-        JSONArray daveCarsResponse = jsonResponse.getBody().getObject().getJSONArray("options");
-        for (int i = 0; i < daveCarsResponse.length(); i++) {
-            JSONObject job = daveCarsResponse.getJSONObject(i);
+        //Iterate over the available cars and select only those that fit the number of passengers
+        JSONArray carsResponse = jsonResponse.getBody().getObject().getJSONArray("options");
+        for (int i = 0; i < carsResponse.length(); i++) {
+            JSONObject job = carsResponse.getJSONObject(i);
             CarPrice cp = new CarPrice(job.getString("car_type"), job.getInt("price"), jsonResponse.getBody().getObject().getString("supplier_id"));
             if (carCapacityMap.get(job.getString("car_type")) >= passengersNo) {
                 carList.add(cp);
@@ -138,8 +146,10 @@ public class RidesAppConsole {
         //System.out.println("Sorted list: " + carList);
     }
 
+    //Choose the cheapest option for each car type
     public static List<CarPrice> chooseBestCars(List<CarPrice> carList) {
         List<CarPrice> bestCars = new ArrayList<>();
+        //The last element must be part of bestCars, as it is the cheapest
         bestCars.add(carList.get(carList.size() - 1));
         for (int i = carList.size() - 2; i >= 0; i--) {
             boolean foundCarType = false;
@@ -148,6 +158,7 @@ public class RidesAppConsole {
                     foundCarType = true;
                 }
             }
+            //Only the first different car type found
             if (!foundCarType) {
                 bestCars.add(carList.get(i));
             }
